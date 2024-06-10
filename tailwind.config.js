e6334c1de681e322e31d84bac34e0dcda1a6ceab
dainji4get <ndai31072004@gmail.com>
2024-06-10 14:47:35 +0700

    create database.json, make a flash sale, countdown clock,...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#DB4444",
+        grayScale: "#f5f5f5",
+      },
+    },
   },
   variants: {
     extend: {},
