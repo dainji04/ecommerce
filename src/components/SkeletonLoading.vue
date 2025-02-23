a0ebfa59896e73398d5bbe86edbaaf3410286ec3
dainji04 <ndai31072004@gmail.com>
2025-02-23 20:40:46 +0700

    skeleton loading mobile screen

--- src/components/SkeletonLoading.vue
+++ src/components/SkeletonLoading.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="max-w-[1140px] mx-auto flex justify-between max-tablet:grid-cols-2"
+    class="max-w-[1140px] mx-auto flex justify-between max-tablet:grid max-tablet:mx-5 max-tablet:gap-4 max-tablet:grid-cols-2"
   >
     <div v-for="index in 4" class="grid">
-      <div class="skeleton--loading w-[266px] h-[250px] bg-[#d9d9d9]"></div>
+      <div
+        class="skeleton--loading w-[266px] max-tablet:w-[160px] h-[250px] max-tablet:h-[170px] bg-[#d9d9d9]"
+      ></div>
       <div>
         <div
           class="skeleton--loading w-full h-7 bg-[#d9d9d9] mt-2 rounded-md"
